c2b9435d8019cf637b2a6577d3e14f14866e935f
tahhh <thadpongsermsook1@gmail.com>
2022-01-14 10:21:11 +0700

    lab16_4 completed

--- lab16_4.cpp
+++ lab16_4.cpp
@@ -17,3 +17,34 @@ int main(){
 	
 	return 0;
 }
+
+void shuffle(int &A,int &B,int &C,int &D){
+    int a[] = {A, B, C, D}, b[4] = {};
+
+    int i = rand()% 4;
+    b[0] = a[i];
+    a[i] = 0;
+
+    int j = 0;
+    do{
+        j = rand()%4;
+    }while(a[j] == 0) ;
+    b[1] = a[j];
+    a[j] = 0;
+
+    int k = 0;
+    do{
+        k = rand()% 4;
+    }while(a[k] == 0) ;
+    b[2] = a[k];
+    a[k] = 0;
+
+    int l = 0;
+    do{
+        l = rand()% 4;
+    }while(a[l] == 0) ;
+    b[3] = a[l];
+    a[l] = 0;
+
+    A = b[0], B = b[1], C =b[2], D = b[3];
+}
